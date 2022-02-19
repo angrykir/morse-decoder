bc90af2dcbeb950a92af5ee8dfd6efd3835f3441
Kirill Maksimuk <angrykir@gmail.com>
2022-02-20 02:26:09 +0300

    feat: solved the task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,42 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const decimalArr = expr.match(/.{10,10}/g);
+
+    const morzeArr = decimalArr.map(e => e.match(/.{2,2}/g));
+
+    for (let i = 0; i < morzeArr.length; i++) {
+     for (let j = 0; j < morzeArr[i].length; j++) {
+      if (morzeArr[i][j] === '00') {
+        morzeArr[i][j] = '';
+      } else if (morzeArr[i][j] === '10') {
+        morzeArr[i][j] = '.';
+      } else if (morzeArr[i][j] === '11') {
+        morzeArr[i][j] = '-';
+      }
+    }
+    }
+
+    const morzeArrStr = morzeArr.map(e => e.join(''));
+
+    function getValues(e) {
+      if (e === '**********') {
+        e = ' '
+      } else {
+         e = MORSE_TABLE[`${e}`]
+      }
+      return e;
+    };
+    
+    const wordArrStr = morzeArrStr.map( e => getValues(e));
+    
+    const decodeStr  =  wordArrStr.join('');
+
+    return decodeStr
+
 }
 
 module.exports = {
     decode
 }
+
